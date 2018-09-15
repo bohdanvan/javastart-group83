95eb2f8a82660ea6d3c83d6f8868a068a26b4e99
Bohdan Vanchuhov <bohdan.vanchuhov@globallogic.com>
2018-09-15 11:41:53 +0300

    lessons 5-6

---
+++ src/WeekForExample.java
@@ -0,0 +1,13 @@
+/**
+ * @author bvanchuhov
+ */
+public class WeekForExample {
+
+    public static void main(String[] args) {
+        // for ( 1 ; 2 ; 3 ) { ... }
+
+        for (int day = 1; day <= 5; day++) {
+            System.out.println(day + ": Work");
+        }
+    }
+}
